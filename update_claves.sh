5ea614dfc1b5cc9e3ffac7fe7e9ca452750a3831
MarioQuiroga <marioeduardoquiroga@gmail.com>
2023-11-03 18:14:23 -0400

    Carlos Completo v6

--- update_claves.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Obtén la dirección IP pública de Terraform
-MARIO_CLAVES=$(terraform output My_ip)
-
-# Realiza la sustitución en el archivo de inventario
-sed -i "s/my_ip/$my_ip/g" inventario
--- update_inventory.sh
+++ update_inventory.sh
@@ -3,5 +3,8 @@
 # Obtén la dirección IP pública de Terraform
 my_ip=$(terraform output My_ip)
 
+# Establece una variable de entorno con la dirección IP
+export ARACHNI_IP=$my_ip
+
 # Realiza la sustitución en el archivo de inventario
 sed -i "s/my_ip/$my_ip/g" inventario
