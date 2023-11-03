4f0d4326330d396c3b74620a57cab2b83a46b113
MarioQuiroga <marioeduardoquiroga@gmail.com>
2023-11-03 14:20:58 -0400

    Carlos v1

---
+++ instance.tf
@@ -0,0 +1,61 @@
+resource "aws_instance" "MyInstancia" {
+  ami           = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t3.micro"
+  key_name = "mario_claves"
+  security_groups = [aws_security_group.allow_ssh_http.id]
+  subnet_id = "subnet-0a5ddfb50c67b40ea"
+  tags = {
+    Name = "MyInstancia"
+  }
+}
+
+output "My_ip"{
+  value = aws_instance.MyInstancia.public_ip
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+  name        = "allow_ssh_http"
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["189.146.21.85/32"]
+  }
+
+  ingress {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "webgoat"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "webgoat"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
