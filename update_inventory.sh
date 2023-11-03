cc3dec9e024da30f919b1229551c673b62332b7a
MarioQuiroga <marioeduardoquiroga@gmail.com>
2023-11-03 16:26:57 -0400

    Carlos Completo v2.1

---
+++ update_inventory.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Obtén la dirección IP pública de Terraform
+my_ip=$(terraform output My_ip)
+
+# Realiza la sustitución en el archivo de inventario
+sed -i "s/my_ip/$my_ip/g" inventory.txt
