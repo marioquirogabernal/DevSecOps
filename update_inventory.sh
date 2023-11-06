787fa1fc364155030b116966d91ac6be9df5415d
MarioQuiroga <marioeduardoquiroga@gmail.com>
2023-11-06 18:04:18 -0500

    Hector v1.1.7

--- update_inventory.sh
+++ update_inventory.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Obtén la dirección IP pública de Terraform
-my_ip=$(terraform output My_ip)
+my_ip=$(terraform output My_ip | sed 's/"//g')
 
 # Establece una variable de entorno con la dirección IP
 export ARACHNI_IP=$my_ip
 
 # Realiza la sustitución en el archivo de inventario
+
 sed -i "s/my_ip/$my_ip/g" inventario
